9a270d554c945ef56bf0e96de0577c3ae078b97a
Jared Howard <jared@howardpants.com>
2014-06-10 18:19:46 -0600

    add Introduction to Go presentation

---
+++ introduction-to-go/for.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	println(sum)
+
+	sum = 1
+	for sum < 1000 {
+		sum += sum
+	}
+	println(sum)
+
+	sum = 1
+	for sum < 1000 {
+		sum += sum
+	}
+	println(sum)
+}
---
+++ introduction-to-go/type-map
@@ -0,0 +1,3 @@
+map[string]int
+map[*T]struct{ x, y float64 }
+map[string]interface{}
